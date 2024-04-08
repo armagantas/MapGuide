910d61d4904b1e622246404c706c684f93877c13
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-08 23:43:40 +0300

    Initial Commit

---
+++ MapGuide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapGuide
+//
+//  Created by Armağan Erdem Taş on 8.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapGuide/MapGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapGuideApp.swift
+//  MapGuide
+//
+//  Created by Armağan Erdem Taş on 8.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
