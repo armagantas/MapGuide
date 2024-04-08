28a0b7d1ac34f7ed2e619923500a9d74d0262671
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-08 23:57:11 +0300

    Folder structure has been set. Assets added.

---
+++ MapGuide/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  MapGuide
+//
+//  Created by Armağan Erdem Taş on 8.04.2024.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+
