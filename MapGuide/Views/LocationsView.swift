1767d4482c43858f1ffce0e5f9ae683225513792
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2024-04-09 20:02:46 +0300

    Location preview cards with asymmetric transition.

--- MapGuide/Views/LocationsView.swift
+++ MapGuide/Views/LocationsView.swift
@@ -24,6 +24,18 @@ struct LocationsView: View {
                     .padding()
                 
                 Spacer()
+                
+                ZStack {
+                    ForEach(viewModel.locations){ location in
+                        if viewModel.mapLocation == location {
+                            LocationPreviewView(location: location)
+                                .shadow(color: Color.black.opacity(0.3),
+                                        radius: 20)
+                                .padding()
+                                .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+                        }
+                    }
+                }
             }
         }
         
